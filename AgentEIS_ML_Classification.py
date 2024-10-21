6172a14d3b14f7740d98208ee5bf1d9db51963cb
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2024-10-21 09:00:14 +0800

    Update AgentEIS_ML_Classification.py

--- AgentEIS_ML_Classification.py
+++ AgentEIS_ML_Classification.py
@@ -14,54 +14,46 @@ from impedance.visualization import plot_nyquist
 import joblib
 
 def eis_ML_select(sample_features):
-    # loaded_model = ExtraTreesClassifier(n_estimators=200, max_depth=10, min_samples_split=5)
     loaded_model = joblib.load('ET_model_all.pkl')
-
-    # 在测试集上进行预测并计算准确率
     eis_type = loaded_model.predict(sample_features)
-    # 在测试集上进行预测并计算准确率
     eis_type_prob = loaded_model.predict_proba(sample_features)
-    # 获取概率最高的三个类别的索引
     top_3_indices = eis_type_prob.argsort()[:, -3:][:, ::-1]
-    # 获取概率最高的三个类别的标签值和概率值
     eis_type_top3 = [(loaded_model.classes_[i], eis_type_prob[j][i]) for j, i in enumerate(top_3_indices)]
-    # return eis_type, eis_type_top3
     return eis_type, eis_type_top3
 
 
 def get_model_and_guess(eis_type):
-    if eis_type == 0: #Type1
+    if eis_type == 0: 
         circuit_model = "R_0-p(R_1-W_0,CPE_0)"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0, 1.0]
-    elif eis_type == 1: #Type2
+    elif eis_type == 1: 
         circuit_model = "R_0-p(R_1,CPE_0)-p(R_2,CPE_1)-W_0"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0, 1.0e-1, 1.0, 1.0, 1.0]
-    elif eis_type == 2: #Type3
+    elif eis_type == 2: 
         circuit_model = "R_0-p(R_1,CPE_0)-p(R_2-W_0,CPE_1)"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0, 1.0e-1, 1.0, 1.0, 1.0]
-    elif eis_type == 3: #Type4, #005-Raccichini-Figure4
+    elif eis_type == 3: 
         circuit_model = "R_0-p(R_1,CPE_0)-Wo_0"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0, 1.0, 1.0]
-    elif eis_type == 4: #Type5
+    elif eis_type == 4: 
         circuit_model = "R_0-p(R_1-Wo_0,CPE_0)-CPE_1"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-    elif eis_type == 5: #panasonic
+    elif eis_type == 5: 
         circuit_model = "R_0-p(R_1,C_0)-p(R_2,C_1)-Ws_0"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0e-1, 1.0, 1.0, 1.0]
-    elif eis_type == 6: #46Ah Kokam Nano Pouch
+    elif eis_type == 6: 
         circuit_model = "R_0-p(R_1,C_0)-p(R_2,C_1)"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0e-1, 1.0]
-    elif eis_type == 7: #005-Raccichini
+    elif eis_type == 7: 
         circuit_model = "R_0-CPE_0"
         initial_guess = [1.0e-1, 1.0, 1.0]
-    elif eis_type == 8: #005-Raccichini-Figure4
+    elif eis_type == 8: 
         circuit_model = "R_0-Wo_0"
         initial_guess = [1.0e-1, 1.0, 1.0]
-    elif eis_type == 9: #006
+    elif eis_type == 9: 
         circuit_model = "R_0-p(R_1-W_0,C_0)"
         initial_guess = [1.0e-1, 1.0e-1, 1.0, 1.0]
     else:
-        # 如果输入的数不在1到14之间，抛出一个异常
         raise ValueError("Input type must be between 0 and 9.")
     return circuit_model, initial_guess
 
@@ -118,7 +110,7 @@ def unwrap_dataframe(df):
 def tuples_to_string(tuples):
     return ",".join([f"{d[0]:.9f},{d[1]:.9f}" for d in tuples])
 
-def merge_columns_to_row(df): #将三列压缩进三个格子里
+def merge_columns_to_row(df): 
     df.columns = ['freq', 'zreal', 'zimag']
     A = df.T
     A['E'] = A.apply(lambda row: ','.join([str(val) for val in row]), axis=1)
@@ -155,30 +147,12 @@ def rename_dataframe_columns(df):
     return df
 
 def load_eis_data(file_path):
-    """
-    将包含三列“f, zreal, zimag"的数据转变为ML模型可以处理的dataframe
-    加载 EIS 数据文件，将多列数据合并成一行，并进行数据预处理和重命名列名。
-    :param file_path: EIS 数据文件路径。
-    :return: 处理后的 EIS 数据 DataFrame。
-    """
-    # 加载 EIS 数据文件
     eis_data = pd.read_csv(file_path, sep=',')
-
-    # 合并多列数据成一行
     merged_data = merge_columns_to_row(eis_data)
-
-    # 进行数据预处理
     preprocessed_data = preprocess_data(merged_data)
-
-    # 处理数据
     processed_data = process_data_final(preprocessed_data)
-
-    # 按 No. 分组
     grouped_data = group_dataframe(processed_data)
-
-    # 重命名列名并将数据类型转换为浮点数
     renamed_data = rename_dataframe_columns(grouped_data).astype(float)
-
     return renamed_data
 
 def get_eis_ml_results(file_path):
@@ -188,13 +162,7 @@ def get_eis_ml_results(file_path):
     return eis_type
 
 def get_equivalent_circuit(eis_type):
-    """
-    根据输入的 eis_type 返回对应的电路模型和初始猜测参数。
-
-    :param eis_type: EIS 类型的整数值。
-    :return: 对应的电路模型和初始猜测参数。
-    :raises ValueError: 如果输入的 eis_type 不在预定义的范围内。
-    """
+ 
     get_equivalent_circuit = {
         0: ("R_0-p(R_1-W_0,CPE_0)"),
         1: ("R_0-p(R_1,CPE_0)-p(R_2,CPE_1)-W_0"),
@@ -219,7 +187,7 @@ def plot_nyquist_SZ(Z, scale=1, units='Ohm', fmt='.-', ax=None, labelsize=20,
     Z = np.array(Z, dtype=complex)
 
     if ax is None:
-        _, ax = plt.subplots(figsize=figsize)  # 设置图形的长宽
+        _, ax = plt.subplots(figsize=figsize) 
 
     ax.plot(np.real(Z), -np.imag(Z), fmt, color=color, markersize=markersize, linewidth=linewidth, **kwargs)
 
@@ -252,36 +220,3 @@ def plot_nyquist_SZ(Z, scale=1, units='Ohm', fmt='.-', ax=None, labelsize=20,
     t.set_size(16)
 
     return ax
-
-'''
-sample_path = "100soc-EIS.csv"
-# sample_path = "exampleData-fengxiang.csv"
-sample_data = load_eis_data(sample_path)
-
-eis_type, eis_type_top3 = eis_ML_select(sample_data)
-print("eis_type_best:", eis_type[0])
-print("eis_type_TOP3:", eis_type_top3)
-
-fig, axs = plt.subplots(1, 3, figsize=(32, 8))
-
-for i, et in enumerate(eis_type_top3[0][0]):
-    circuit_model, initial_guess = get_model_and_guess(et)
-    circuit = CustomCircuit(circuit_model, initial_guess=initial_guess)
-
-    freq, Z = readFile(sample_path)
-    freq, Z = ignoreBelowX(freq, Z)
-    circuit.fit(freq, Z)
-
-    circuit_pred = circuit.predict(freq)
-
-    plot_nyquist_SZ(Z, fmt='o', markerfacecolor='none', color='#6495ED', ax=axs[i])
-    plot_nyquist_SZ(circuit_pred, fmt='-', color='#F08080', ax=axs[i])
-
-
-    # axs[i].set_title("EIS Type: {}".format(et))
-
-
-plt.savefig('EIS_plot_output-pouch-2.tiff', dpi=600, format='tiff')
-plt.show()
-
-'''
