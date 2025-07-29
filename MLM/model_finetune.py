3d3ddffcf384f414fe39981f22e1d3b2af31775c
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2025-07-30 07:39:45 +0800

    Update and rename 004model_tuned.py to MLM/model_finetune.py

--- MLM/model_finetune.py
+++ MLM/model_finetune.py
@@ -16,25 +16,19 @@ import swanlab
 import json
 import os
 
-swanlab.login(api_key="Gsky84PhoA8DzRhAQx30p")
+swanlab.login(api_key="...")
 
 prompt = "You are an expert in the field of electrochemistry and need to conduct impedance equivalent circuit analysis. What is the equivalent circuit of the electrochemical impedance in this picture?"
-# model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
-# local_model_path = "./Qwen/Qwen2___5-VL-7B-Instruct"
-# train_dataset_json_path = "eis_dataset_train.json"
-# val_dataset_json_path = "eis_dataset_val.json"
-# output_dir = "./output/Qwen2___5-VL-7B-EIS"
-# MAX_LENGTH = 100000
-model_id = "Qwen/Qwen2.5-VL-3B-Instruct"
-local_model_path = "./Qwen/Qwen2___5-VL-3B-Instruct"
+model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
+local_model_path = "./Qwen/Qwen2___5-VL-7B-Instruct"
 train_dataset_json_path = "eis_dataset_train.json"
 val_dataset_json_path = "eis_dataset_val.json"
-output_dir = "./output/Qwen2___5-VL-3B-EIS"
+output_dir = "./output/Qwen2___5-VL-7B-EIS"
 MAX_LENGTH = 100000
 
 def process_func(example):
     """
-    将数据集进行预处理
+    data_preprocess
     """
     input_ids, attention_mask, labels = [], [], []
     conversation = example["conversations"]
@@ -48,8 +42,8 @@ def process_func(example):
                 {
                     "type": "image",
                     "image": f"{image_file_path}",
-                    "resized_height": 500,
-                    "resized_width": 500,
+                    "resized_height": 512,
+                    "resized_width": 512,
                 },
                 {"type": "text", "text": prompt},
             ],
@@ -57,8 +51,8 @@ def process_func(example):
     ]
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=True
-    )  # 获取文本
-    image_inputs, video_inputs = process_vision_info(messages)  # 获取数据数据（预处理过）
+    )  
+    image_inputs, video_inputs = process_vision_info(messages) 
     inputs = processor(
         text=[text],
         images=image_inputs,
@@ -66,7 +60,7 @@ def process_func(example):
         padding=True,
         return_tensors="pt",
     )
-    inputs = {key: value.tolist() for key, value in inputs.items()} #tensor -> list,为了方便拼接
+    inputs = {key: value.tolist() for key, value in inputs.items()} #tensor -> list,
     instruction = inputs
 
     response = tokenizer(f"{output_content}", add_special_tokens=False)
@@ -82,7 +76,7 @@ def process_func(example):
             + response["input_ids"]
             + [tokenizer.pad_token_id]
     )
-    if len(input_ids) > MAX_LENGTH:  # 做一个截断
+    if len(input_ids) > MAX_LENGTH:  
         input_ids = input_ids[:MAX_LENGTH]
         attention_mask = attention_mask[:MAX_LENGTH]
         labels = labels[:MAX_LENGTH]
@@ -91,13 +85,13 @@ def process_func(example):
     attention_mask = torch.tensor(attention_mask)
     labels = torch.tensor(labels)
     inputs['pixel_values'] = torch.tensor(inputs['pixel_values'])
-    inputs['image_grid_thw'] = torch.tensor(inputs['image_grid_thw']).squeeze(0)  #由（1,h,w)变换为（h,w）
+    inputs['image_grid_thw'] = torch.tensor(inputs['image_grid_thw']).squeeze(0)  
     return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels,
             "pixel_values": inputs['pixel_values'], "image_grid_thw": inputs['image_grid_thw']}
 
 
 def predict(messages, model):
-    # 准备推理
+    
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=True
     )
@@ -111,7 +105,7 @@ def predict(messages, model):
     )
     inputs = inputs.to("cuda")
 
-    # 生成输出
+    
     generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
     generated_ids_trimmed = [
         out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
@@ -123,41 +117,34 @@ def predict(messages, model):
     return output_text[0]
 
 
-# 在modelscope上下载Qwen2.5-VL模型到本地目录下
 model_dir = snapshot_download(model_id, cache_dir="./", revision="master")
 
-# 使用Transformers加载模型权重
 tokenizer = AutoTokenizer.from_pretrained(local_model_path, use_fast=False, trust_remote_code=True)
 processor = AutoProcessor.from_pretrained(local_model_path)
 
 origin_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     local_model_path, 
-    device_map="cuda",  # 将auto改为cuda，强制使用CUDA设备
+    device_map="cuda",  
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
-).to("cuda")  # 添加显式设备转移
-origin_model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+).to("cuda")  
+origin_model.enable_input_require_grads() 
 
-
-# 处理数据集：读取json文件
 train_ds = Dataset.from_json(train_dataset_json_path)
 train_dataset = train_ds.map(process_func)
 
-# 配置LoRA
 config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-    inference_mode=False,  # 训练模式
-    r=4,  # Lora 秩
-    lora_alpha=16,  # Lora alaph，具体作用参见 Lora 原理
-    lora_dropout=0.05,  # Dropout 比例
+    inference_mode=False,  
+    r=4,  
+    lora_alpha=16,  # Lora alaph，
+    lora_dropout=0.05,  
     bias="none",
 )
 
-# 获取LoRA模型
 train_peft_model = get_peft_model(origin_model, config)
 
-# 配置训练参数
 args = TrainingArguments(
     output_dir=output_dir,
     per_device_train_batch_size=4,
@@ -172,12 +159,11 @@ args = TrainingArguments(
     report_to="none",
 )
     
-# 设置SwanLab回调
 swanlab_callback = SwanLabCallback(
-    project="Qwen2.5-VL-3B-eis",
-    experiment_name="3B-eis",
+    project="Qwen2.5-VL-eis",
+    experiment_name="eis",
     config={
-        "model": "https://modelscope.cn/models/Qwen/Qwen25-VL-3B-Instruct",
+        "model": "https://modelscope.cn/models/Qwen/Qwen25-VL-7B-Instruct",
         "dataset": "https://modelscope.cn/datasets/AI-ModelScope/eis/summary",
         # "github": "https://github.com/datawhalechina/self-llm",
         "model_id": model_id,
@@ -194,7 +180,6 @@ swanlab_callback = SwanLabCallback(
 )
 
 
-# 配置Trainer
 trainer = Trainer(
     model=train_peft_model,
     args=args,
@@ -203,32 +188,29 @@ trainer = Trainer(
     callbacks=[swanlab_callback],
 )
 
-# 开启模型训练
+
 trainer.train()
 
-# ====================测试===================
-# 配置测试参数
+# ====================test===================
+
 val_config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-    inference_mode=True,  # 训练模式
-    r=4,  # Lora 秩
-    lora_alpha=16,  # Lora alaph，具体作用参见 Lora 原理
-    lora_dropout=0.05,  # Dropout 比例
+    inference_mode=True,  
+    r=4,  
+    lora_alpha=16,  
+    lora_dropout=0.05,  
     bias="none",
 )
 
-# 获取测试模型，从output_dir中获取最新的checkpoint
 load_model_path = f"{output_dir}/checkpoint-{max([int(d.split('-')[-1]) for d in os.listdir(output_dir) if d.startswith('checkpoint-')])}"
 print(f"load_model_path: {load_model_path}")
 val_peft_model = PeftModel.from_pretrained(origin_model, model_id=load_model_path, config=val_config)
 
-# 读取测试数据
 with open(val_dataset_json_path, "r") as f:
     test_dataset = json.load(f)
 
 test_image_list = []
-# 新增结果记录列表
 results = []
 
 for item in test_dataset:
@@ -241,8 +223,8 @@ for item in test_dataset:
             {
             "type": "image", 
             "image": image_file_path,
-            "resized_height": 500,
-            "resized_width": 500,   
+            "resized_height": 512,
+            "resized_width": 512,   
             },
             {
             "type": "text",
@@ -255,7 +237,6 @@ for item in test_dataset:
     print(f"predict:{response}")
     print(f"gt:{label}\n")
 
-    # 将结果添加到列表
     results.append({
         "image_path": image_file_path,
         "predict": response,
@@ -264,8 +245,7 @@ for item in test_dataset:
     
     test_image_list.append(swanlab.Image(image_file_path, caption=response))
 
-# 保存到CSV文件
-csv_path = "./prediction_results_qwenVL2_5-3B.csv"
+csv_path = "./prediction_results_qwenVL2_5-7B.csv"
 with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['image_path', 'predict', 'gt']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -274,5 +254,4 @@ with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
 
 swanlab.log({"Prediction": test_image_list})
 
-# 在Jupyter Notebook中运行时要停止SwanLab记录，需要调用swanlab.finish()
 swanlab.finish()
