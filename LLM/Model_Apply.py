d05ca8e8904551e15e41f18e84b0929fdf902d87
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2025-08-05 19:12:38 +0800

    Update Model_Apply.py

--- LLM/Model_Apply.py
+++ LLM/Model_Apply.py
@@ -16,23 +16,17 @@ def predict(messages, model, tokenizer):
 
     return response
 
-
-# 加载原下载路径的tokenizer和model
 tokenizer = AutoTokenizer.from_pretrained("./Qwen/Qwen3-8B", use_fast=False, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained("./Qwen/Qwen3-8B", device_map="auto", torch_dtype=torch.bfloat16)
 
-# 加载训练好的Lora模型，将下面的checkpointXXX替换为实际的checkpoint文件名名称
 model = PeftModel.from_pretrained(model, model_id="./output/Qwen3-8B-small/checkpoint-67")
 
-# 加载测试数据集（替换原有测试数据）
-test_df = pd.read_json("D:/AgentEIS_4/new_test.jsonl", lines=True)
+test_df = pd.read_json(".../test.jsonl", lines=True)
 
-# 批量预测函数
 def batch_predict(df, model, tokenizer):
     results = []
     from tqdm import tqdm
-    # 创建 output-small 文件夹
-    output_folder = "output-small-8B"
+    output_folder = "output-Qwen3-8B"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
@@ -46,17 +40,16 @@ def batch_predict(df, model, tokenizer):
             "Original": row['output'],
             "Prediction": prediction
         })
-        # 将每个 Prediction 保存为 txt 文件
         file_path = os.path.join(output_folder, f"prediction_{index}.txt")
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(prediction)
     
     return pd.DataFrame(results)
 
-# 执行批量预测并保存结果
 results_df = batch_predict(test_df, model, tokenizer)
 results_df.to_csv(
-    "D:/AgentEIS_4/test_results-8B-small.csv", 
+    ".../results.csv", 
     index=False,
     encoding='utf-8-sig'
 )
+
